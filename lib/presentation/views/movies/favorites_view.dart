724f6a2ced57d050a5d4ae652d4c5266732ede18
Daniel Alejandro Gimenez <danielalejandro347@gmail.com>
2024-11-23 02:48:16 -0300

    If there's not favorites movies it shows a message indicating this situation.

--- lib/presentation/views/movies/favorites_view.dart
+++ lib/presentation/views/movies/favorites_view.dart
@@ -1,7 +1,8 @@
-import 'package:cinemapedia/presentation/widgets/widgets.dart';
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:cinemapedia/domain/entities/movie.dart';
+import 'package:cinemapedia/presentation/widgets/widgets.dart';
 import 'package:cinemapedia/presentation/providers/providers.dart';
 
 class FavoritesView extends ConsumerStatefulWidget {
@@ -38,6 +39,32 @@ class FavoritesViewState extends ConsumerState<FavoritesView> {
   Widget build(BuildContext context) {
     final List<Movie> favoriteMovies =
         ref.watch(favoriteMoviesProvider).values.toList();
+
+    if (favoriteMovies.isEmpty) {
+      final colors = Theme.of(context).colorScheme;
+
+      return Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Icon(Icons.favorite_outline_sharp, size: 60, color: colors.primary),
+            Text(
+              'Ohhh no!!',
+              style: TextStyle(fontSize: 30, color: colors.primary),
+            ),
+            const Text(
+              'No tienes películas favoritas',
+              style: TextStyle(fontSize: 20, color: Colors.black45),
+            ),
+            const SizedBox(height: 20),
+            FilledButton.tonal(
+                onPressed: () => context.go('/home/0'),
+                child: const Text('Empieza a buscar'))
+          ],
+        ),
+      );
+    }
 
     return Scaffold(
       body: MovieMasonry(
